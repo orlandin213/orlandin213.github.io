58b24b0ce13b410fa4d6be83b52840cc8248d247
orlandin213 <orlandonf88@gmail.com>
2024-09-04 06:17:19 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FooterComponent } from './pages/footer/footer.component';
-import { HeaderComponent } from './pages/header/header.component';
-import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },  // Ruta predeterminada
-  { path: 'footer', component: FooterComponent },
-  { path: 'header', component: HeaderComponent },
-  { path: 'home', component: HomeComponent },
+
   { path: '**', redirectTo: 'home', pathMatch: 'full' }  // Ruta comodín
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { FooterComponent } from './pages/footer/footer.component';
-import { HeaderComponent } from './pages/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    FooterComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
